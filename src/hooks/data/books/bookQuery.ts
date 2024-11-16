08d0d8cf73d889ce12b8538dfd1d3b3b76db54fe
MontassarSN <snenmontassar@gmail.com>
2024-11-16 22:54:14 +0100

    backend integration review

--- src/hooks/data/books/bookQuery.ts
+++ src/hooks/data/books/bookQuery.ts
@@ -5,7 +5,7 @@ import { IBookPopulated } from "@/types";
 const bookQuery = (bookId : string) => ({
   queryKey: ["books",bookId],
   queryFn: async () => {
-    const url = await getEndpoint({  resourse: "books", action: "getBookId" });
+    const url =  getEndpoint({  resourse: "books", action: "getBookId" });
     return await CRUDData<IBookPopulated>({ method: "GET", url: url(bookId) });
     
   },
