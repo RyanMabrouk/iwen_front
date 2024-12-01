1ed70fb179d0c0cebf02ad214c0494df0c02704d
RaayeN3 <158215143+RaayeN3@users.noreply.github.com>
2024-12-01 10:11:57 +0100

    1/12

--- src/components/main/banners/Banners.tsx
+++ src/components/main/banners/Banners.tsx
@@ -1,7 +1,12 @@
 "use client";
+import CustomSwiper from "@/app/ui/Swiper";
 import useBanners from "@/hooks/data/banners/useBanners";
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
+
+type Banner = {
+  url: string;
+};
 
 export default function Banners() {
   const { data: banners } = useBanners() as unknown as {
@@ -10,13 +15,29 @@ export default function Banners() {
 
   return (
     <div className="relative sm:px-6 sm:py-24 lg:px-8 lg:py-36 [&_.swiper-pagination-bullet-active]:bg-primary-200">
-      <Image
-        src={(banners?.data?.length && (banners?.data)[0].url) || ""}
-        alt="Banner Image"
-        layout="responsive"
-        width={200}
-        height={500}
-        className="mx-auto w-full max-w-[100%] rounded-xl sm:h-[350px] sm:max-w-[80%]"
+      <CustomSwiper
+        slides={(banners?.data || []).map((banner: Banner, index: number) => (
+          <Image
+            key={index}
+            src={banner.url || ""}
+            alt="Banner Image"
+            layout="responsive"
+            width={200}
+            height={500}
+            className="mx-auto w-full max-w-[100%] rounded-xl sm:h-[350px] sm:max-w-[80%]"
+          />
+        ))}
+        initialSlide={0}
+        slidesPerView={1}
+        pagination
+        loop
+        allowTouchMove
+        speed={1500}
+        autoplay={{
+          delay: 5000,
+          disableOnInteraction: true,
+        }}
+        className=""
       />
     </div>
   );
