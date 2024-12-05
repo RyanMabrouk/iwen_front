f99da493002aaa615525559648870f70c3cca08e
Rayen Mabrouk <mabroukr999@gmail.com>
2024-12-06 00:47:29 +0100

    refactor: Uncomment necessary imports and components in layout for improved functionality

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,13 @@
 import type { Metadata } from "next";
 import { Tajawal } from "next/font/google";
 import "./globals.css";
-// import Hydration from "@/provider/MainHydration";
-// import { ToastContainer, ToastProvider } from "@/hooks/useToast";
-// import { SpeedInsights } from "@vercel/speed-insights/next";
-// import { Analytics } from "@vercel/analytics/react";
-// import Store from "@/provider/QCStore";
+import Hydration from "@/provider/MainHydration";
+import { ToastContainer, ToastProvider } from "@/hooks/useToast";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
+import Store from "@/provider/QCStore";
 import React, { Suspense } from "react";
 import Footer from "./(dashboard)/ui/Footer";
-// import { Player } from "@lottiefiles/react-lottie-player";
 
 const tajawal = Tajawal({
   weight: ["200", "300", "400", "500", "700", "800", "900"],
@@ -41,32 +40,21 @@ export default async function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
       <body className={tajawal.className + " min-h-screen"}>
-        {/* <Analytics />
+        <Analytics />
         <SpeedInsights />
         <Store>
           <Hydration>
             <ToastProvider>
-              <ToastContainer /> */}
-        <Suspense
-          fallback={
-            "Loading..."
-            // <Player
-            //   className="m-auto"
-            //   autoplay
-            //   loop
-            //   src="/loading.json"
-            //   style={{ height: "12rem", width: "12rem" }}
-            // />
-          }
-        >
-          <main>
-            {children}
-            <Footer />
-          </main>{" "}
-        </Suspense>
-        {/* </ToastProvider>
+              <ToastContainer />
+              <Suspense fallback={"loading..."}>
+                <main>
+                  {children}
+                  <Footer />
+                </main>{" "}
+              </Suspense>
+            </ToastProvider>
           </Hydration>
-        </Store> */}
+        </Store>
       </body>
     </html>
   );
