184bff308ad47b35949b7c5e7e2ab29f31d66cee
Rayen Mabrouk <mabroukr999@gmail.com>
2024-12-06 00:55:42 +0100

    refactor: Add experimental config for Suspense CSRBailout and remove "use client" directive from layout

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   compiler: {
     removeConsole: process.env.NODE_ENV !== "development", // Remove console.log in production
   },
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
   swcMinify: true,
   logging: {
     fetches: {
