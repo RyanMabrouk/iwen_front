a69c8ad94054486fa1db6bd998de03e207121ea5
Rayen Mabrouk <mabroukr999@gmail.com>
2024-12-06 02:04:14 +0100

    feat: Add loading components for auth and dashboard, and update layout files

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,12 +1,8 @@
+"use client";
 import Header from "@/components/Header/Header";
 import React from "react";
-import NavBar from "./ui/NavBar";
-import Footer from "./ui/Footer";
-export default async function Layout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Header />
