0d750ab617c22b98b94560355f999eeff8b1498c
Rayen Mabrouk <mabroukr999@gmail.com>
2024-12-06 01:35:00 +0100

    refactor: Uncomment Analytics and SpeedInsights components in RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,8 +41,8 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
       <body className={tajawal.className + " min-h-screen"}>
-        {/* <Analytics />
-        <SpeedInsights /> */}
+        <Analytics />
+        <SpeedInsights />
         <Store>
           <Hydration>
             <ToastProvider>
