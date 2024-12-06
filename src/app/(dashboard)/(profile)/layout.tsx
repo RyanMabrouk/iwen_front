fe351bf3d82a543ec94dee9867727a9fab0d365c
Rayen Mabrouk <mabroukr999@gmail.com>
2024-12-06 16:38:12 +0100

    refactor: Wrap layout component with Suspense to enhance loading experience with a fallback animation

--- src/app/(dashboard)/(profile)/layout.tsx
+++ src/app/(dashboard)/(profile)/layout.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { Suspense } from "react";
 import Header from "./ui/header";
 import NavZone from "./ui/navZone";
 import getUser from "@/api/getUser";
 import { redirect } from "next/navigation";
+import { Player } from "@lottiefiles/react-lottie-player";
 export default async function Layout({
   children,
 }: {
@@ -13,9 +14,21 @@ export default async function Layout({
     redirect("/login");
   }
   return (
-    <div className=" m-auto mt-12 flex h-full min-h-screen w-full flex-col overflow-x-hidden bg-bgcolor1 p-10">
-      <Header />
-      <NavZone>{children}</NavZone>
-    </div>
+    <Suspense
+      fallback={
+        <Player
+          className="m-auto"
+          autoplay
+          loop
+          src="/loading.json"
+          style={{ height: "12rem", width: "12rem" }}
+        />
+      }
+    >
+      <div className="m-auto mt-12 flex h-full min-h-screen w-full flex-col overflow-x-hidden bg-bgcolor1 p-10">
+        <Header />
+        <NavZone>{children}</NavZone>
+      </div>
+    </Suspense>
   );
 }
