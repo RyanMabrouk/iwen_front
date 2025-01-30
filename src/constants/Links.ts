bfda8306726baae6eacb42527a77c8f286c38901
MontassarSN <snenmontassar@gmail.com>
2025-01-30 17:46:46 +0100

    fix: Update contact information and improve phone link format in Socials component

--- src/constants/Links.ts
+++ src/constants/Links.ts
@@ -1,9 +1,9 @@
-const PHONE_NUMBER = "123-456-7890";
-const EMAIL = "iwen@gamil.com";
-const FACEBOOK_URl = "https://www.facebook.com/";
-const TWITTER_URl = "https://twitter.com/";
-const INSTAGRAM_URl = "https://www.instagram.com/";
-const ADDRESS = "تونس ـ شارع أبو قاسم الشابي 4084";
+const PHONE_NUMBER = "+212775703996";
+const EMAIL = "dariwaan.2023@gmail.com";
+const FACEBOOK_URl = "https://www.facebook.com/profile.php?id=61571977282243&locale=fr_FR";
+const TWITTER_URl = "https://x.com/DariwaanD";
+const INSTAGRAM_URl = "https://www.instagram.com/khmysdryn/";
+const ADDRESS = "شارع القسطلاني الحبوس الدار البيضاء";
 
 export {
   PHONE_NUMBER,
