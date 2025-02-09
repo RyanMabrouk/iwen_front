7a132ab720620aec5329b9f8163da1b75e580509
Rayen Mabrouk <mabroukr999@gmail.com>
2025-02-09 22:44:54 +0100

    feat: Set output to "export" in next.config.js for improved build configuration

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   compiler: {
     removeConsole: process.env.NODE_ENV !== "development", // Remove console.log in production
   },
